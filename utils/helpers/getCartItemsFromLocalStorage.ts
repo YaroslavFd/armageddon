43a413cde5ddc75854d78915a056c42b09f9b392
Yaroslav <yaroslav.web9@gmail.com>
2023-08-26 15:08:47 +0300

    small fix

--- utils/helpers/getCartItemsFromLocalStorage.ts
+++ utils/helpers/getCartItemsFromLocalStorage.ts
@@ -1,4 +1,7 @@
 export const getCartItemsFromLocalStorage = (): CardItem[] => {
-  const itemsJSON = localStorage.getItem('cartItems');
-  return itemsJSON ? (JSON.parse(itemsJSON) as CardItem[]) : [];
+  if (typeof window !== 'undefined') {
+    const itemsJSON = localStorage.getItem('cartItems');
+    return itemsJSON ? (JSON.parse(itemsJSON) as CardItem[]) : [];
+  }
+  return [];
 };
